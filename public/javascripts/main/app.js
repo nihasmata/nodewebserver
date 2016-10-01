1ce4b9b990349556a016b39f470cea56ede58e72
Sahin Atam <Sahin@Sahins-MacBook-Pro.local>
2016-10-02 01:22:18 +0300

    first commit on node server

---
+++ public/javascripts/main/app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Sahin on 02/10/16.
+ */
+
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+
+app.get('/', function (req, resp) {
+    resp.send('This is the Sahin\'s firs web server');
+});
+
+app.get('/about', function (req, resp) {
+    resp.send('We are the crazy computer engineers!');
+});
+
+app.get('/bio', function (req, resp) {
+    resp.send('I am from Elazığ which located in the east part of Turkey and i born in 1989 and graduated' +
+        ' from Marmara University in 2011');
+});
+
+
+app.listen(PORT, function () {
+    console.log('Web server started on the port :' + PORT);
+})
